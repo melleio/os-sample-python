4998b0b00b6dee49c4fd75e9345be0bc7a246d3f
Lloyd <lloyd@emelle.me>
2017-08-02 19:24:33 -0400

    updates

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,13 @@
 from flask import Flask
+from time import sleep
+import re
+import requests
+import csv, os, json
+import html5lib
+from lxml import html
+from flask import Flask, render_template, request
+from bs4 import BeautifulSoup
+
 application = Flask(__name__)
 
 @application.route("/")
